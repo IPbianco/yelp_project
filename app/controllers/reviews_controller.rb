3cb7b3d4a423b687fcf2f7f00363af3e821753d4
Tabish Raza <tabishraza@gmail.com>
2017-12-12 19:20:45 +0000

    methods for routing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,25 @@
 class ReviewsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
-    @what = 'text'
-    # render html: '<p>test</p>'
-    @reviews = Review.all
+    # @reviews = Review.all
+    review = Review.last
+    @reviews = review.title
   end
 
-  def show
-    render html: '<p>test</p>'
+  def new
+
+  end
+
+  def create
+    @review = Review.create(title: params[:title],
+                        description: params[:description],
+                        rating: params[:rating], user_id: 1, restaurant_id: 1)
+    p @review
+    # if @review.id.nil?
+    #   p "jkahsdjkhs",@review
+    #   render :action => "new"
+    # else
+      redirect_to '/reviews'
+    # end
   end
 end
