df804c69fc50da11cbb5b6687f228344d2ef7ee6
Tabish Raza <tabishraza@gmail.com>
2017-12-13 17:24:08 +0000

    see all restaurants, user id attached to create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,15 +2,24 @@ class RestaurantsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @restaurants = Restaurant.all.reverse
+    if user_signed_in?
+      @restaurants = Restaurant.all.reverse
+    else
+      redirect_to ("/users/sign_in")
+    end
   end
 
   def new
 
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
+  end
+
   def create
-    Restaurant.create(name: params[:name], description: params[:description], user_id: 1)
+    Restaurant.create(name: params[:name], description: params[:description], user_id: current_user.id)
     redirect_to ("/restaurants")
   end
 
