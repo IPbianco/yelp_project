d6930a9dd2c90d17ea56f85aefdcdecbb3a3f38a
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-12-13 12:11:01 +0000

    Adds feature test for new restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+names = %w[Paul Peter Mike Linda Rose John Mary Nicole Jack Frida]
+
+10.times do |index|
+  user = User.create(name: "#{names[index]}", email: "#{names[index]}_0#{index}@gmail.com", password: "secret_password")
+  restaurant = Restaurant.create(name: "Majd's Fried Chicken (#{index})", description: "Nice food", user_id: user.id)
+end
+
+reviews = 20.times.map do |index|
+  rando = rand(1..9)
+  Review.create(title: "Nice place", description: "Majd is great", rating: rando - 1, user_id: rando, restaurant_id: rando + 1)
+end
--- spec/features/add_restaurant_spec.rb
+++ spec/features/add_restaurant_spec.rb
@@ -1,6 +1,20 @@
+require 'rails_helper'
+
 feature "Adding new restaurant" do
-  scenario "See the form to enter data" do
+
+  scenario "Enter restaurant name" do
     visit("/restaurants/new")
-    expect(page).to have_content("Welcome")
+    fill_in "name", with: "Ignacio Fish n Chips"
+    fill_in "description", with: "Best in town, free for Majd"
+    click_on("Add")
+    expect(page).to have_content("Ignacio Fish n Chips")
+  end
+
+  scenario "Enter restaurant description" do
+    visit("/restaurants/new")
+    fill_in "name" , with: "Ignacio Fish n Chips"
+    fill_in "description" , with: "Best in town, free for Majd"
+    click_on("Add")
+    expect(page).to have_content("Best in town, free for Majd")
   end
 end
