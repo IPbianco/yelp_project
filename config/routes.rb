0419efd74fa8c44d2738674c7b7597ec865a5a5c
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-12-13 15:19:15 +0000

    breaks everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+devise_for :users, controllers: { sessions: 'users/sessions' }
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+root to: 'users#index'
 
 resources :reviews, only: [:index, :new, :create]
+# resources :users
 # root 'reviews#index'
 # get '/reviews/index', to: 'reviews#index'
 # get '/reviews/new', to: 'reviews#new'
@@ -12,4 +14,5 @@ resources :reviews, only: [:index, :new, :create]
 
 
 resources :restaurants, only: [:index, :new, :create]
+
 end
