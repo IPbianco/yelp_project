6b1e51eb133f424bdf8077058ede3ca4b51ae2fc
Tabish Raza <tabishraza@gmail.com>
2017-12-13 12:09:38 +0000

    test for adding review

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
+  # before(:all) do
+  #   user = User.create(name: "Eva", email: "baguette@frenchy.com", password:"123")
+  #   Restaurant.create(name: "Eva's", description: "baguettes ONLY", user_id: user.id)
+  # end
+
 feature 'Create a review' do
   scenario 'fill in the form' do
-    visit("/review/new")
-    fill_in 'title', :with 'title'
-    fill_in 'description', :with 'description'
-    fill_in 'rating', :with 5
-    click_button 'submit'
-    expect(page).to have_content('test')
+    user = User.create(name: "Eva", email: "baguette@frenchy.com", password:"123")
+    Restaurant.create(name: "Eva's", description: "baguettes ONLY", user_id: user.id)
+    visit("/reviews/new")
+    fill_in 'title', with: 'title'
+    fill_in 'description', with: 'description'
+    fill_in 'rating', with: 5
+    click_button 'Submit'
+    expect(page).to have_content('title')
   end
 end
