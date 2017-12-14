1883d0932d3d642bbac569da0d68f80cc6c1bf1f
majdeddine <majda-jambouri@hotmail.fr>
2017-12-14 16:32:15 +0000

    load json data for restaurant reviews

--- public/js/review.js
+++ public/js/review.js
@@ -1,6 +1,24 @@
 "use strict";
 
 window.addEventListener("load", function() {
-  
 
+  function getId(location){
+    return parseInt(location.pathname.split("/").pop())
+  }
+
+  var currentRestaurantId = getId(window.location);
+
+  function getReviews(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+
+      if (request.readyState == 4 && request.status == 200) {
+        var data = JSON.parse(request.response).reviews;
+        console.log(data);
+      }
+    }
+    request.open("GET", `/restaurants/${currentRestaurantId}/reviews`, true)
+     request.send(null);
+  }
+  getReviews()
 });
