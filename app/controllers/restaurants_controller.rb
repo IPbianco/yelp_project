b8542b521b8e7b3e7b888060e844ff763b71c0df
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-12-14 15:21:03 +0000

    Adds review.html and js files

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,6 @@
 class RestaurantsController < ApplicationController
+
   skip_before_action :verify_authenticity_token
-
-
 
   def index
     if user_signed_in?
@@ -11,20 +10,12 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  def new
-
-  end
-
   def show
-    @restaurant = Restaurant.find(params[:id])
-    @reviews = @restaurant.reviews
+    render file: Rails.public_path.join("templates","one_restaurant.html"), layout: true
   end
-
-
 
   def create
       new_player = Player.new(player_params)
-
       if new_player.save
         render json: { player: new_player }
       else
@@ -33,13 +24,8 @@ class RestaurantsController < ApplicationController
     end
 
   def create
-
-
     restaurant = Restaurant.create(name: params[:name], description: params[:description], user_id: current_user.id)
     imagge = Image.create(source: params[:image], restaurant_id: restaurant.id)
-    p '::::::::::' , imagge
-
     redirect_to ("/restaurants")
   end
-
 end
