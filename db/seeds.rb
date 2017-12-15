bf5910455e4b46938b8c32d3e55757a72a748ab1
Tabish Raza <tabishraza@gmail.com>
2017-12-15 10:46:18 +0000

    adding images to restaurants for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,10 @@ names = %w[Paul Peter Mike Linda Rose John Mary Nicole Jack Frida]
 10.times do |index|
   user = User.create(name: "#{names[index]}", email: "#{names[index]}_0#{index}@gmail.com", password: "secret_password")
   restaurant = Restaurant.create(name: "Majd's Fried Chicken (#{index})", description: "Nice food", user_id: user.id)
+  Image.create(source: Rails.root.join("public/uploads/image/source/2/oleg.jpg").open, restaurant_id: restaurant.id)
 end
 
 reviews = 20.times.map do |index|
   rando = rand(1..9)
   Review.create(title: "Nice place", description: "Majd is great", rating: rando - 1, user_id: rando, restaurant_id: rando + 1)
 end
-
-user = User.create(name: "Eva", email: "baguette@frenchy.com", password:"123")
-restaurant = Restaurant.create(name: "Eva's", description: "baguettes ONLY", user_id: user.id)
