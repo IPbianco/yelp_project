faaffca24a2f4996c95bb9ba894cd259a60159ea
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-12-15 11:33:10 +0000

    Adds reviews page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,15 +19,6 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-      new_player = Player.new(player_params)
-      if new_player.save
-        render json: { player: new_player }
-      else
-        render json: { errors: new_player.errors }, status: 500
-      end
-    end
-
-  def create
     restaurant = Restaurant.create(name: params[:name], description: params[:description], user_id: current_user.id)
     imagge = Image.create(source: params[:image], restaurant_id: restaurant.id)
     redirect_to ("/restaurants")
--- public/js/review.js
+++ public/js/review.js
@@ -2,22 +2,50 @@
 
 window.addEventListener("load", function() {
 
-  var currentRestaurantId
-  var request
-  var data
-  var contentDiv
+  var currentRestaurantId,
+      reviewsArray,
+      data,
+      restaurantName,
+      title,
+      description,
+      rating;
 
   function getId(location){
     return parseInt(location.pathname.split("/").pop())
   }
 
+  function getRestaurantName() {
+    $.get(`/restaurants/${currentRestaurantId}.json`, function(data) {
+      restaurantName = data.name
+      $("#content").prepend(`<h1>${restaurantName}</h1><br>`)
+    })
+  }
+
   function getReviews() {
     $.get(`/restaurants/${currentRestaurantId}/reviews`, function(data) {
-      console.log(data)
+      reviewsArray = data.reviews
+      reviewsArray.forEach(function(review) {
+        $("#reviewsList").append(`<li>${review.title}</li>`)
+        $("#reviewsList").append(`<li>${review.description}</li><br>`)
+      })
+    })
+  }
+
+  function addNewReviewField() {
+    $("#create").click(function (event) {
+      event.preventDefault()
+      title = $("#reviewTitle").val()
+      description = $("#reviewDescription").val()
+      rating = $("#reviewRating").val()
+      $.post(`/restaurants/${currentRestaurantId}/reviews`, { title: `${title}`, description: `${description}`, rating: `${rating}`, restaurant: `${currentRestaurantId}`}, function() {
+        document.getElementById("reviewsList").innerHTML = ""
+        getReviews()
+      })
     })
   }
 
   currentRestaurantId = getId(window.location);
-  contentDiv = document.getElementById("content");
+  getRestaurantName()
   getReviews()
+  addNewReviewField()
 });
