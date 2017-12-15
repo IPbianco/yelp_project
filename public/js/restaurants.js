c585c0605e2ad1ea09f6bdd99340f8179027cad6
Tabish Raza <tabishraza@gmail.com>
2017-12-15 10:45:34 +0000

    many olegs for a seed

--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
   function restaurantsGet() {
     var index = 1
     $.get('http://localhost:3000/restaurants', function(data){
+      console.log(data);
       data.forEach(function(element) {
       eachRestaurant(element, index)
       index++;
@@ -14,16 +15,14 @@ $(document).ready(function() {
    });
   }
 
-  // function eachRestaurant(element, index) {
-  //   var container = index
-  //   $('#restaurants').append("<div id="+container+" class='container' style='margin-top: 20;'</div>");
-  //   // $("#"+container).append("<div class=</div>")
-  //   $("#"+container).append("<div class='image'><img style='width:240px; height:160px;border-radius:10px' src= "+ element['images']['source']['source']['url'] +"/><</div>")
-  //   $("#"+container).append("<div class='name'>" + element.name + "</div>")
-  //   // $("#"+container).append("<div class='location test'><b href=\"#\">" + element.location + "</b></div>")
-  //   // $("#"+container).append("<div class='price'><c href=\"#\">" + "£" + element.price + "</c></div>")
-  //   // $("#"+container).append("<div class='capacity'><d href=\"#\">" + "Guests: "+ element.capacity + "</d></div>")
-  //   // $("#"+container).append("<form class='rentalButton' action='/rental/save' method='post'><e href=\"#\"><button id='more-info"+element.id+"' type='submit' class='button' name='id' value="+element.id+" class='rental_id'>More Info</button></e></form>")
-  //
-  // };
+  function eachRestaurant(element, index) {
+    var container = index
+    $('#restaurants').append("<div id="+container+" class='container' style='margin-top: 20;'</div>");
+    // $("#"+container).append("<div class=</div>")
+    $("#"+container).append("<div class='image'><img style='width:240px; height:160px;border-radius:10px' src= "+ element.images['source']['url'] +"/></div>")
+    $("#"+container).append("<div class='name'>" + element.name + "</div>")
+    $("#"+container).append("<div class='description'>" + element.description + "</div>")
+    $("#"+container).append(`<form class='moreInfo' action='/restaurants/${element.id}' method='get'><button type='submit' value="Submit">More Info</button></form>`)
+
+  };
   });
